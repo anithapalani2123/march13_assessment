a0c5be3db5d4b5fd5608f0455b8089b0b186fdea
ANITHA P <94184990+anithapalani2123@users.noreply.github.com>
2023-03-14 09:25:53 +0530

    Add files via upload

---
+++ march14/src/square_pattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class square_pattern {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print
+                ("Enter the Number:");
+        int a=sc.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            for(int j=1;j<=a;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
